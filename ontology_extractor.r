aefa6c7401374c42b306f184b32051fc5f7ea226
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2013-02-21 20:40:03 +0100

    First draft

---
+++ ontology_extractor.r
@@ -0,0 +1,19 @@
+file <- read.csv(file="repos/DataDump/csv/BRT_1889_02_T1.csv", head=FALSE, sep=",")
+df <- data.frame(file)
+
+# Rows removal
+df <- df[-1:-3,]
+
+# Cols removal
+df$V1 = NULL
+df <- df[,-7:-nrow(df)]
+
+# Replace "Id." by the item just above
+for (i in 1:nrow(df)) {
+if (df$V5[i] == 'id.' | df$V5[i] == 'Id.' | df$V5[i] == 'id') {df$V5[i] = df$V5[i-1]}
+}
+
+# Remove all rows with totals
+df <- df[-grep("Totaal*", df$V5),]
+df <- df[-grep("totaal*", df$V5),]
+
